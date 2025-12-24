521c6307090d9d07a2fc073f5841cc10e868abae
Naman <namanagar12@gmail.com>
2025-12-24 17:33:16 +0530

    Completed swap_and_delete

--- 1000_rated/swap_and_delete_1913B.cpp
+++ 1000_rated/swap_and_delete_1913B.cpp
@@ -3,12 +3,38 @@
 using namespace std;
 
 void solve() {
-    int n;
-    cin>>n;
+    string s;
+    cin>>s;
+    int n = s.length();
+    if(n==0 || n==1) {
+        cout<<n<<'\n';
+        return;
+    }
 
-    //starting
-    
-
+    int cnt_0=0, cnt_1=0;
+    for(char c : s) {
+        if(c == '0') cnt_0++;
+        else cnt_1++;
+    }
+    int i=0;
+    while(i<n && cnt_0!=0 && cnt_1!=0) {
+        if(s[i]=='0') {
+            cnt_1--;
+        } else {
+            cnt_0--;
+        }
+        i++;
+    }
+    while(i<n && cnt_0!=0 && s[i]=='1') {
+        cnt_0--;
+        i++;
+    }
+    while(i<n && cnt_1!=0 && s[i]=='0') {
+        cnt_1--;
+        i++;
+    }
+    cout<<n - i<<'\n';
+    return;
 }
 
 int main() {
